b6370132453adf772efb01efa4a274a01ace75ce
yongsin0129 <yongsin0129@gmail.com>
2022-06-21 16:17:12 +0800

    feat : add express-handlebars

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const exphbs = require('express-handlebars').engine
+
 const app = express()
 const port = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
 app.listen(port, () => {
