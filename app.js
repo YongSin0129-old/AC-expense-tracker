cfd765b5f6281ecb38ab9d28c66781844ea0f09d
yongsin0129 <yongsin0129@gmail.com>
2022-06-21 16:37:31 +0800

    feat : add routes folder and modules

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars').engine
+const router = require('./routes')
 
 const app = express()
 const port = 3000
@@ -8,9 +9,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.set('views', './views')
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+app.use(router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
