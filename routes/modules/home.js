5d8c0678a648185710acad6563a01259f30cba7a
ysRedhouse <yongsin0129@gmail.com>
2022-06-23 14:51:08 +0800

    feat : render home page with records data

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const recordsModel = require('../../models/records')
 
+// 首頁
 router.get('/', (req, res) => {
-  res.render('home')
+  const userId = req.user._id
+  const sortMethod = req.query.sort || '_id'
+  recordsModel.find({ userId })
+    .sort(sortMethod)
+    .lean()
+    .then(records => {
+      res.render('home', { records })
+    })
+    .catch(error => {
+      console.log(error)
+    })
 })
 
 module.exports = router
