eeb1811ecfa9e51ad21d77c1fc792ee6c4178d39
ysRedhouse <yongsin0129@gmail.com>
2022-06-23 14:17:08 +0800

    feat : add Fackbook login feature

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const { authenticator } = require('../middleware/auth')
 const home = require('./modules/home')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
