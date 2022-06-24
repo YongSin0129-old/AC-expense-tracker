122224dc82a4213db661d29e912efaed50d98687
yongsin0129 <yongsin0129@gmail.com>
2022-06-24 09:41:27 +0800

    feat : add create new record router

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -3,11 +3,21 @@ const router = express.Router()
 const recordsModel = require('../../models/records')
 const categoriesModel = require('../../models/categories')
 
+// 新增 record
 router.get('/new', async (req, res) => {
   const categories = await categoriesModel.find().lean()
   res.render('new', { categories })
 })
 
+router.post('', async (req, res) => {
+  const userId = req.user._id
+  req.body.userId = userId
+  await recordsModel.create(req.body)
+  req.flash('success_msg', '成功新增餐廳')
+  res.redirect('/')
+})
+
+// 修改 record
 router.get('/:id/edit', async (req, res, next) => {
   const userId = req.user._id
   const recordId = req.params.id
