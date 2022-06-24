7b285b7ad215a3113ab0c2b4d2d281e906eedd76
yongsin0129 <yongsin0129@gmail.com>
2022-06-24 10:02:10 +0800

    feat : calculate totalAmount

--- routes/modules/index.js
+++ routes/modules/index.js
@@ -8,16 +8,24 @@ router.get('/', async (req, res) => {
   const userId = req.user._id
   const records = await recordsModel.find({ userId }).lean()
   const categories = await categoriesModel.find().lean()
+
   records.forEach(record => {
-    record.date = record.date.toJSON().toString().slice(0, 10)
+    record.date = record.date
+      .toJSON()
+      .toString()
+      .slice(0, 10)
     record.icon = categories.find(category => {
       // 這邊不能使用 === 來確認，需使用 equals() - Compares the equality of this ObjectId with otherID.
       return category._id.equals(record.categoryId)
     }).icon
   })
+
+  const totalAmount = records.reduce((pre, cur) => pre + cur.amount, 0)
+
   res.render('index', {
     records,
-    categories
+    categories,
+    totalAmount
   })
 })
 
